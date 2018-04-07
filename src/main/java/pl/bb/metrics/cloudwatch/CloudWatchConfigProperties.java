8728da027170ba618c89e8a620f3554f25c0b887
bb.bartosz <bb.bartosz@gmail.com>
2018-04-07 15:58:27 +0200

    simplified class hierarchy. validation for properties

--- src/main/java/pl/bb/metrics/cloudwatch/CloudWatchConfigProperties.java
+++ src/main/java/pl/bb/metrics/cloudwatch/CloudWatchConfigProperties.java
@@ -1,27 +1,49 @@
 package pl.bb.metrics.cloudwatch;
 
 import io.micrometer.cloudwatch.CloudWatchConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
-@Service
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+
+@ConfigurationProperties(value = "management.metrics.export.cloudwatch",ignoreUnknownFields = false)
+@Validated
 class CloudWatchConfigProperties implements CloudWatchConfig {
+    @NotEmpty
+    private String namespace;
 
-    @Autowired
-    private CloudWatchProperties properties;
+    @PositiveOrZero
+    private int batchSize;
 
-    @Override
-    public String get(String key) {
-        return null;
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
     }
 
     @Override
     public String namespace() {
-        return properties.getNamespace();
+        return namespace;
     }
 
     @Override
     public int batchSize() {
-        return properties.getBatchSize();
+        return batchSize;
+    }
+
+    @Override
+    public String get(String key) {
+        return null;
     }
 }
--- src/main/java/pl/bb/metrics/cloudwatch/CloudwatchConfiguration.java
+++ src/main/java/pl/bb/metrics/cloudwatch/CloudwatchConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties(CloudWatchProperties.class)
+@EnableConfigurationProperties(CloudWatchConfigProperties.class)
 @Configuration
 class CloudwatchConfiguration {
 
